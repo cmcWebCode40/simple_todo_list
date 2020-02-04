d5757bed785a8da4194c50bad0dd06272bd00670
cmcWebCode40 <infinity_michael@yahoo.com>
2020-02-04 17:04:39 +0100

    update on service worker

--- src-sw.js
+++ src-sw.js
@@ -1,11 +1,5 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
-//custom Adjustments..
-
-// workbox.routing.register(
-//   new RegExp("https://jsonplaceholder.typicode.com/users"),
-//   workbox.strategies.cacheFirst()
-// );
 
 workbox.preaching.preacheAndRoute([]);
